7309627b74558af0acd32c0fe1b030ebc5bcd163
Sudharsun <psu.fabulo@gmail.com>
2022-01-06 21:38:31 +0530

    started making product cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screen/HomeScreen";
 
 function App() {
 	return (
@@ -8,7 +9,7 @@ function App() {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<h1>Project Shop</h1>
+					<HomeScreen />
 				</Container>
 
 				<Footer />
