e2021ebea330e497bf114a80cf59a9f74e997d94
Sudharsun <psu.fabulo@gmail.com>
2022-01-07 01:31:45 +0530

    starting express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-const hello = "hello";
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("api is running");
+});
+
+app.get("/api/products", (req, res) => {
+	res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
+const portNum = 5000;
+app.listen(portNum, console.log(`Server running at port ${portNum}`));
