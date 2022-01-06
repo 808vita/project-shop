d4b9fe3968cbd8a63ca8b8bdd1239685bd37be86
Sudharsun <psu.fabulo@gmail.com>
2022-01-07 02:35:01 +0530

    doing express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
@@ -16,5 +19,8 @@ app.get("/api/products/:id", (req, res) => {
 	res.json(product);
 });
 
-const portNum = 5000;
-app.listen(portNum, console.log(`Server running at port ${portNum}`));
+const PORT = process.env.PORT || 5000;
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
