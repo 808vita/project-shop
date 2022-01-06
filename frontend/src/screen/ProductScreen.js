946de2180e75ec1453bb12803e50ad98f7c02728
Sudharsun <psu.fabulo@gmail.com>
2022-01-07 02:07:39 +0530

    starting express

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = () => {
 	let params = useParams();
-	const product = products.find((p) => p._id === params.id);
+
+	const [product, setProduct] = useState({});
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${params.id}`);
+			setProduct(data);
+		};
+		fetchProduct();
+	}, []);
+
 	return (
 		<>
 			<Link className="btn btn-dark my-2 " to="/">
