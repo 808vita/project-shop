82f1603283003d18b48af1038a2c300af28708c2
Sudharsun <psu.fabulo@gmail.com>
2022-01-10 04:30:21 +0530

    name update in nav bar

--- frontend/src/screen/ProfileScreen.js
+++ frontend/src/screen/ProfileScreen.js
@@ -6,6 +6,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 
 import { getUserDetails, updateUserProfile } from "../actions/userActions";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 const ProfileScreen = () => {
 	const history = useNavigate();
@@ -31,14 +32,15 @@ const ProfileScreen = () => {
 		if (!userInfo) {
 			history("/login");
 		} else {
-			if (!user.name) {
+			if (!user.name || success) {
+				dispatch({ type: USER_UPDATE_PROFILE_RESET });
 				dispatch(getUserDetails("profile"));
 			} else {
 				setName(user.name);
 				setEmail(user.email);
 			}
 		}
-	}, [dispatch, user, userInfo, history, setName, setEmail]);
+	}, [dispatch, user, userInfo, history, setName, setEmail, success]);
 
 	const submitHandler = (e) => {
 		e.preventDefault();
