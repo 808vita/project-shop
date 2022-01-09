2898265ac3632c569061847413292b3fef22b506
Sudharsun <psu.fabulo@gmail.com>
2022-01-10 04:09:45 +0530

    done profile screen | fixed issues product screen

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-	Container,
 	Row,
 	Col,
 	Image,
