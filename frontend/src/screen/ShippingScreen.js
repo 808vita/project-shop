cf18ad8422e85b7c7d824cebbb632ca0262da40a
Sudharsun <psu.fabulo@gmail.com>
2022-01-10 20:40:15 +0530

    shipping screen | checkout steps

--- frontend/src/screen/ShippingScreen.js
+++ frontend/src/screen/ShippingScreen.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
 const ShippingScreen = () => {
-	const params = useParams();
 	const history = useNavigate();
 
 	const cart = useSelector((state) => state.cart);
@@ -27,6 +27,7 @@ const ShippingScreen = () => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 
 			<Form onSubmit={submitHandler}>
