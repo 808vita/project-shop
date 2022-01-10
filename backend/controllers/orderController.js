822fb5e72ec1e792c302ea858eb241781a5298ae
Sudharsun <psu.fabulo@gmail.com>
2022-01-11 02:15:41 +0530

    started order fetching from db

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,4 +37,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems };
+// @des get order by id
+// @route  get  /api/orders/:id
+// @access private
+const getOrderById = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id).populate(
+		"user",
+		"name email"
+	);
+
+	if (order) {
+		res.json(order);
+	} else {
+		res.status(404);
+		throw new Error("Order not found");
+	}
+});
+
+export { addOrderItems, getOrderById };
